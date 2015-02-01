157f9e4f15bd20a48e11d8d330b512d6feefbe63
Bianca Rosa <me@biancarosa.com.br>
2015-02-01 21:25:44 -0200

    Update main.c

--- trab-so/main.c
+++ trab-so/main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include<semaphore.h>
-#include<string.h>
+#include <semaphore.h>
+#include <string.h>
 
 #define nLerCatalogo         1
 #define nLerArquivos         3
 #define nMultiplicarMatrizes 4
 #define nDeterminanteMatriz  5
 #define nEscreverArquivo     3
-#define OUT_FILE "saida.out"
+#define bufferSize           5
 
 
 
 int debug, contLeitura, contEscrita;
+
+sem_t mutexEscrita;
 
 typedef double **mat;
 
@@ -25,7 +27,7 @@ typedef struct {
 } S;
 
 typedef struct{
-    S *buffer[5];
+    S *buffer[bufferSize];
     sem_t full, empty, mutex;
     int bufferIn, bufferOut;
 } sharedStruct;
@@ -87,9 +89,13 @@ mat newMatrix(int size) {
 mat cloneMatrix( mat matrix, int size ){
     mat clone = newMatrix( size );
     int i, j;
-    for( i=0; i<size; i++ ){
-        for( j=0; j<size; j++ ){
-            clone[i][j] = matrix[i][j];
+    #pragma omp parallel shared( matrix, clone, size ) private(i,j)
+    {
+        #pragma omp for schedule(static)
+        for( i=0; i<size; i++ ){
+            for( j=0; j<size; j++ ){
+                clone[i][j] = matrix[i][j];
+            }
         }
     }
     return clone;
@@ -97,9 +103,13 @@ mat cloneMatrix( mat matrix, int size ){
 
 void zeroMatrix(mat matrix, int size) {
     int i, j;
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++) {
-            matrix[i][j] = 0;
+    #pragma omp parallel shared( matrix, size ) private(i,j)
+    {
+        #pragma omp for schedule(static)
+        for (i = 0; i < size; i++) {
+            for (j = 0; j < size; j++) {
+                matrix[i][j] = 0;
+            }
         }
     }
 }
@@ -120,7 +130,7 @@ int multiplyMatrices(mat matA, mat matB, mat resolt, int size){
     omp_set_num_threads(2);
     #pragma omp parallel shared(matA,matB,resolt) private(i,j,k) 
     {
-        #pragma omp for  schedule(static)
+        #pragma omp for schedule(static)
         for (i=0; i<size; i=i+1){
             for (j=0; j<size; j=j+1){
                 resolt[i][j]=0.;
@@ -133,92 +143,42 @@ int multiplyMatrices(mat matA, mat matB, mat resolt, int size){
     return 0;
 }
 
-void mat_pivot(mat a, mat p, int size) {
+double determinant(mat matrix, int size) {
+    double det = 1;
     int i, j, k;
-    double aux;
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++) {
-            p[i][j] = (i == j);
-        }
-    }
-    for (i = 0; i < size; i++) {
-        int max_j = i;
-        for (j = i; j < size; j++) {
-            if (fabs(a[j][i]) > fabs(a[max_j][i])) max_j = j;
-        }
-        if (max_j != i)
-            for (k = 0; k < size; k++) {
-                aux = p[i][k];
-                p[i][k] = p[max_j][k];
-                p[max_j][k] = aux;
-            }
-    }
-}
-
-void mat_LU(mat matrix, mat matL, mat matU, mat matP, int size) {
-    int i, j, k;
-    mat Aprime = newMatrix(size);
-    double s;
+    mat matL = newMatrix(size), matU = newMatrix(size);
     zeroMatrix(matL, size);
     zeroMatrix(matU, size);
-    mat_pivot(matrix, matP, size);
-    multiplyMatrices(matP, matrix, Aprime, size);
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++) {
-            if (j <= i) {
-                s = 0;
-                for (k = 0; k < size; k++) {
-                    s += matL[j][k] * matU[k][i];
-                }
-                matU[j][i] = Aprime[j][i] - s;
-            }
-            if (j >= i) {
-                s = 0;
-                for (k = 0; k < size; k++) {
-                    s += matL[j][k] * matU[k][i];
-                }
-                matL[j][i] = (double) ((Aprime[j][i] - s) / matU[i][i]);
-            }
-        }
-    }
-    for (i = 0; i < size; i++) {
-        matL[i][i] = 1;
-    }
-
-}
-
-double triangularMatrixDeterminant(mat matrix, int size) {
-    int i;
-    double det = 1;
-    for (i = 0; i < size; i++) {
-        det *= matrix[i][i];
-    }
-    return det;
-}
-
-double determinant(mat matrix, int size) {
-    double det;
-    int i, pivotCount = 0;
-    mat matL = newMatrix(size), matU = newMatrix(size), matP = newMatrix(size);
     omp_set_dynamic(0);     // Explicitly disable dynamic teams
     omp_set_num_threads(2);
-    #pragma omp parallel shared(matrix, matL, matU, matP, size) private(i)
+    #pragma omp parallel shared( matrix, matL, matU, size) private( i, j, k)
     {
-        mat_LU(matrix, matL, matU, matP, size);
-        det = triangularMatrixDeterminant(matU, size);
-    
-        for (i = 0; i < size; i++) {
-            if (matP[i][i] == 0) {
-                pivotCount++;
+        #pragma omp for schedule( static, 10 )
+        for( k=0; k<size; k++ ){
+            matL[k][k] = 1;
+            for( i=k+1; i<size; i++ ){
+                matL[i][k] = matrix[i][k] / matrix[k][k];
+                for( j=k+1; j<size; j++ ){
+                    matrix[i][j] = matrix[i][j] - matL[i][k] * matrix[k][j];
+                }
+                #pragma omp flush( matrix )
+            }
+            #pragma omp nowait
+            for( j=k; j<size; j++ ){
+                matU[k][j] = matrix[k][j];
             }
         }
-        if (pivotCount % 2 == 0 && pivotCount != 0) {
-            det *= -1;
+        for (i = 0; i < size; i++) {
+            det *= matrix[i][i];
         }
     }
+    
+    //printf( "\n\ndet: %lf\n\n", det );
+    //mat_show( matU, size );
+    //mat_show( matL, size );
+
     free(matL);
     free(matU);
-    free(matP);
     return det;
 }
 
@@ -235,30 +195,39 @@ void lerCatalogo(){
         printf("\n\nErro na leitura do catalogo, entrada.in nao encontrado.\n\n");
         exit(EXIT_FAILURE);        
     }
+ 
     while ((read = getline(&line, &len, fp)) != -1) {
         //printf("%s", line);
         if (read > 100 ){
             printf("\n\nErro na leitura do catalogo, maximo de 100 caracteres no nome do arquivo.\n\n");
             exit(EXIT_FAILURE);        
         }
+        
+        if( contLeitura < 0 ){
+            contLeitura = 0;
+        }
         contLeitura++;
+
         if (read > 1 ){
+            //printf("\nesperando empty lerCatalogo");
             sem_wait(&shared[0].empty);
+            //printf("\nesperando mutex lerCatalogo");
             sem_wait(&shared[0].mutex);
+            //printf("\nescrita lerCatalogo");
             
             
 
             strtok( line, "\n" );
-            for( i=0; i<read; i++ ){
-                shared[0].buffer[shared[0].bufferIn]->Nome[i] = line[i];
-            }
-            
+            strcpy( shared[0].buffer[shared[0].bufferIn]->Nome, line );
+            //for( i=0; i<bufferSize; i++ ){
+               // printf("\n arquivo %s",shared[0].buffer[i]->Nome);
+           // }
             
 
             //printf("%s", shared[0].buffer[shared[0].bufferIn]->Nome);
            // sem_getvalue( &shared[0].empty, &debug);
            // printf("\n\nempty:%d\n\n", debug);
-            shared[0].bufferIn = (shared[0].bufferIn+1)%5;
+            shared[0].bufferIn = (shared[0].bufferIn+1)%bufferSize;
             
             sem_post(&shared[0].full);
             sem_post(&shared[0].mutex);
@@ -275,30 +244,26 @@ void lerArquivos(){
     size_t len = 0;
     ssize_t read;
     int index = 0, i, j, ordem;
-    S *temp = ( S* )malloc( sizeof( S ) );
+    S *temp;
     while( 1 ){
         line = NULL;
         len = 0;
         index = 0;
-        //printf("\n\nEsperando full\n\n");
-        //sem_getvalue( &shared[0].full, &debug);
-        //printf("\n\nfull:%d\n\n", debug);
+        //printf("\nesperando full lerArquivos");
         sem_wait(&shared[0].full);
-        //printf("\n\nEsperando mutex\n\n");
+        //printf("\nesperando mutex lerArquivos");
         sem_wait(&shared[0].mutex);
-        //printf("\n\nEsperando mais nada\n\n");
-        
+        //printf("\nleitura lerArquivos");
+        temp = ( S* )malloc( sizeof( S ) );        
         fp = fopen(shared[0].buffer[shared[0].bufferOut]->Nome, "r");
-        printf("\n\n*%s*\n\n", shared[0].buffer[shared[0].bufferOut]->Nome);
         if (fp == NULL){
             printf("\n\nErro na leitura do arquivo %s.\n\n", shared[0].buffer[shared[0].bufferOut]->Nome);
             exit(EXIT_FAILURE);        
         }
-        //temp->Nome = shared[0].buffer[shared[0].bufferOut]->Nome;
-        for( i=0; i<100; i++ ){
-            temp->Nome[i] = shared[0].buffer[shared[0].bufferOut]->Nome[i];
-        }
-        shared[0].bufferOut = ( shared[0].bufferOut +1 )%5;
+
+        strcpy( temp->Nome, shared[0].buffer[shared[0].bufferOut]->Nome );        
+        
+        shared[0].bufferOut = ( shared[0].bufferOut +1 )%bufferSize;
 
         sem_post(&shared[0].empty);
         sem_post(&shared[0].mutex);
@@ -321,11 +286,22 @@ void lerArquivos(){
             }
         }
         fclose(fp);
-
+        //printf("\nesperando empty lerArquivos");
         sem_wait(&shared[1].empty);
+        //printf("\nesperando mutex lerArquivos");
         sem_wait(&shared[1].mutex);
-        shared[1].buffer[shared[1].bufferIn] = temp;        
-        shared[1].bufferIn  = ( shared[1].bufferIn + 1 )%5;
+        //printf("\nescrita lerArquivos");
+        shared[1].buffer[shared[1].bufferIn] = temp;
+
+
+        //printf("\n\n%s   %d", temp->Nome, shared[1].bufferIn);
+        //printf("\nbufferDump lerArquivos\n\n");
+        //for( i=0; i<bufferSize; i++ ){
+            //rintf("\n%d arquivo %s",i ,shared[1].buffer[i]->Nome);
+        //}
+        temp = NULL;
+
+        shared[1].bufferIn = (shared[1].bufferIn+1)%bufferSize;
         sem_post(&shared[1].full);
         sem_post(&shared[1].mutex);
         
@@ -335,37 +311,40 @@ void lerArquivos(){
 /*MM - Thread Multiplicadora de Matrizes - Move shared[1]->buffer[out] para um ponteiro temporário, calcula C=A*B no elemento temporário e move o ponteiro temporário para shared[2]->buffer[in]. Teremos 4 instâncias desta thread.*/
 
 void multiplicarMatrizes(){
-    S *temp = ( S* )malloc( sizeof( S ) );
+    S *temp;
     int i;
     while(1){
-        printf("\n\nEsperando full 1\n\n");
+        //printf("\nesperando full multiplicarMatrizes");
         sem_wait(&shared[1].full);
-        printf("\n\nEsperando mutex 1\n\n");
+        //printf("\nesperando mutex multiplicarMatrizes");
         sem_wait(&shared[1].mutex);
-        printf("\n\nEsperando mais nada 1\n\n");
+        //printf("\nleitura multiplicarMatrizes");
 
-        int ordem = shared[1].buffer[shared[1].bufferOut]->Ordem;
-        //temp->Nome = shared[1].buffer[shared[1].bufferOut]->Nome;
-        
-        for( i=0; i<100; i++ ){
-            temp->Nome[i] = shared[1].buffer[shared[1].bufferOut]->Nome[i];
-        }
-        
-
+        temp = ( S* )malloc( sizeof( S ) );
+        int ordem = shared[1].buffer[shared[1].bufferOut]->Ordem;        
+        strcpy( temp->Nome, shared[1].buffer[shared[1].bufferOut]->Nome );
         temp->Ordem = ordem;
         temp->A = cloneMatrix ( shared[1].buffer[shared[1].bufferOut]->A, ordem );
         temp->B = cloneMatrix ( shared[1].buffer[shared[1].bufferOut]->B, ordem );
         temp->C = cloneMatrix ( shared[1].buffer[shared[1].bufferOut]->C, ordem );
         multiplyMatrices(temp->A, temp->B, temp->C, ordem);
+        shared[1].bufferOut = ( shared[1].bufferOut +1 )%bufferSize;
 
+
+        
         sem_post(&shared[1].empty);
         sem_post(&shared[1].mutex);
 
+        
+        //printf("\nesperando empty multiplicarMatrizes");
         sem_wait(&shared[2].empty);
+        //printf("\nesperando mutex multiplicarMatrizes");
         sem_wait(&shared[2].mutex);
-        shared[2].buffer[shared[2].bufferIn] = temp;
-        shared[1].bufferOut = ( shared[1].bufferOut +1 )%5;
-        shared[2].bufferIn =  ( shared[2].bufferIn + 1 )%5;
+        //printf("\nescrita multiplicarMatrizes");
+
+        shared[2].buffer[shared[2].bufferIn] = temp;            
+        shared[2].bufferIn =  ( shared[2].bufferIn + 1 )%bufferSize;
+
         sem_post(&shared[2].full);
         sem_post(&shared[2].mutex);
     }
@@ -377,37 +356,40 @@ void multiplicarMatrizes(){
 
 
 void determinanteMatriz(){
-    S *temp = ( S* )malloc( sizeof( S ) );
+    S *temp;
     int i;
     while(1){
-        printf("\n\nEsperando full 2\n\n");
+        //printf("\nesperando full determinanteMatriz");
         sem_wait(&shared[2].full);
-        printf("\n\nEsperando mutex 2\n\n");
+        //printf("\nesperando mutex determinanteMatriz");
         sem_wait(&shared[2].mutex);
-        printf("\n\nEsperando mais nada 2\n\n");
+        //printf("\nleitura determinanteMatriz");
 
+        temp = ( S* )malloc( sizeof( S ) );
         int ordem = shared[2].buffer[shared[2].bufferOut]->Ordem;
-
-        for( i=0; i<100; i++ ){
-            temp->Nome[i] = shared[2].buffer[shared[2].bufferOut]->Nome[i];
-        }
-        printf("\n\ndetermiant %s\n\n", temp->Nome);
+        strcpy( temp->Nome, shared[2].buffer[shared[2].bufferOut]->Nome );
         temp->Ordem = ordem;
         temp->A = cloneMatrix ( shared[2].buffer[shared[2].bufferOut]->A, ordem );
         temp->B = cloneMatrix ( shared[2].buffer[shared[2].bufferOut]->B, ordem );
         temp->C = cloneMatrix ( shared[2].buffer[shared[2].bufferOut]->C, ordem );
         temp->E = determinant(temp->C, ordem);
 
-        
-
+        //printf("\n\n bufferDump determinanteMatriz\n\n");
+        //for( i=0; i<bufferSize; i++ ){
+            //printf("\n arquivo %s",shared[2].buffer[i]->Nome);
+        //}
+        shared[2].bufferOut = ( shared[2].bufferOut +1 )%bufferSize;
         sem_post(&shared[2].empty);
         sem_post(&shared[2].mutex);
 
+        //printf("\nesperando empty determinanteMatriz");
         sem_wait(&shared[3].empty);
+        //printf("\nesperando mutex determinanteMatriz");
         sem_wait(&shared[3].mutex);
+        //printf("\nescrita determinanteMatriz");
         shared[3].buffer[shared[3].bufferIn] = temp;
-        shared[2].bufferOut = ( shared[2].bufferOut +1 )%5;
-        shared[3].bufferIn =  ( shared[3].bufferIn + 1 )%5;
+        temp = NULL;
+        shared[3].bufferIn =  ( shared[3].bufferIn + 1 )%bufferSize;
         sem_post(&shared[3].full);
         sem_post(&shared[3].mutex);
     }
@@ -418,22 +400,25 @@ void determinanteMatriz(){
 /*EA - Thread Escritora de Arquivo - Escreve um arquivo nome.out (nome = nome de entrada). O arquivo contém o nome do arquivo de entrada, a ordem das matrizes e os valores de A, B, C e E. Depois de imprimir, faz free na estrutura S.  Teremos 3 instâncias desta thread.*/
 
 void escreverArquivo(){
+    char arquivoSaida[100];
+    int i;
     while(1){
-        printf("\n\nEsperando full 3\n\n");
+
+        //printf("\nesperando full escreverArquivo");
         sem_wait(&shared[3].full);
-        printf("\n\nEsperando mutex 3\n\n");
+        //printf("\nesperando mutex escreverArquivo");
         sem_wait(&shared[3].mutex);
-        printf("\n\nEsperando mais nada 3\n\n");
+        //printf("\nescrita escreverArquivo");
+
 
         S *temp =  shared[3].buffer[shared[3].bufferOut];
-        shared[3].bufferOut = ( shared[3].bufferOut +1 )%5;
+        shared[3].bufferOut = ( shared[3].bufferOut +1 )%bufferSize;
 
-
-        printf("\n\n Arquivo %s\n",temp->Nome);
-        printf("\n\n Ordem %d\n",temp->Ordem);
-
-
-        FILE * f = fopen(OUT_FILE, "a");
+        
+        strcpy( arquivoSaida,"" );
+        strcat( arquivoSaida, temp->Nome);
+        strcat( arquivoSaida, ".out");
+        FILE * f = fopen(arquivoSaida, "wb");
         if (f == NULL) {
             printf("Error opening file!\n");
             exit(1);
@@ -453,18 +438,15 @@ void escreverArquivo(){
         fprintf(f,"================================\n");
 
         fclose(f);
-
-        contEscrita++;
-
-        printf("Escrita %d  Leitura %d\n", contEscrita, contLeitura);
-
-        if (contEscrita == contLeitura) {
-            printf("acabou \n");
-        };
-
+    
         sem_post(&shared[3].empty);
         sem_post(&shared[3].mutex);
-
+        
+        //printf("\nesperando mutexEscrita escreverArquivo");
+        sem_wait(&mutexEscrita);
+        //printf("\ncontEscrita++");
+        contEscrita++;
+        sem_post(&mutexEscrita);
     }
 
 }
@@ -476,7 +458,7 @@ void initShared(){
         shared[i].bufferOut = 0;
         //shared[i].full, shared[i].empty, shared[i].mutex;
         sem_init(&shared[i].mutex, 1, 1);
-        sem_init(&shared[i].empty, 1, 5);
+        sem_init(&shared[i].empty, 1, bufferSize);
         sem_init(&shared[i].full, 1, 0);
         for( j=0;j<5;j++ ){
             shared[i].buffer[j] = ( S* )malloc( sizeof( S ) );
@@ -499,11 +481,11 @@ int main() {
 
     initShared();
 
-    index = 0;
-    contLeitura = 0;
+    contLeitura = -1;
     contEscrita = 0;
     
-    while ((index<nLerCatalogo) || (index<nLerArquivos) || ( index < nMultiplicarMatrizes ) ) {
+    while( ( index < nLerCatalogo) || ( index < nLerArquivos ) || ( index < nMultiplicarMatrizes )
+                                   || ( index < nDeterminanteMatriz ) || ( index < nEscreverArquivo )   ) {
         if ( index < nLerCatalogo){
             tLerCatalogo[index]=index;
        		pthread_create(&idLerCatalogo, NULL, lerCatalogo, &tLerCatalogo[index]);
@@ -524,34 +506,42 @@ int main() {
             tEscreverArquivo[index]=index;
             pthread_create(&idEscreverArquivo, NULL, escreverArquivo, &tEscreverArquivo[index]);
         }
-
+        index++;
         //if (index<numThreads){
         //    threadArray[index]=index;
         //    pthread_create(&idthread, NULL, threadFunc, &threadArray[index]);
         //}
-        index++;
     }
-
-   // printf("abc");
-
+    
+    sem_init(&mutexEscrita, 1, 1);
     while(1) {
-        if (contEscrita == contLeitura) {
-           // int retvalue = pthread_cancel(idLerCatalogo);
-           // printf("Retorno / thread_cancel :: %d \n",retvalue);
+        //printf("\nesperando mutexEscrita main");
+        sem_wait(&mutexEscrita);
+        sem_wait( &shared[0].mutex );
+        //printf("\n\n%d..%d\n\n", contEscrita, contLeitura);
+        if (contEscrita == contLeitura && contLeitura >=0 ) {
+            //printf("\nmatando tudo\n\n");
+            sem_post(&mutexEscrita);
+            //printf("\n\nAcabou!!! %d..%d\n\n", contEscrita, contLeitura);
+
             int retvalue = pthread_cancel(idEscreverArquivo);
-            printf("Retorno / thread_cancel :: %d \n",retvalue);
-            retvalue = pthread_cancel(idLerArquivos);
-            printf("Retorno / thread_cancel :: %d \n",retvalue);
+            //printf("Retorno / thread_cancel EscreverArquivo:: %d \n",retvalue);
             retvalue = pthread_cancel(idDeterminanteMatriz);
-            printf("Retorno / thread_cancel :: %d \n",retvalue);
+            //printf("Retorno / thread_cancel DeterminanteMatriz:: %d \n",retvalue);
             retvalue = pthread_cancel(idMultiplicarMatrizes);
-            printf("Retorno / thread_cancel :: %d \n",retvalue);
+            //printf("Retorno / thread_cancel MultiplicarMatrizes:: %d \n",retvalue);
+            retvalue = pthread_cancel(idLerArquivos);
+            //printf("Retorno / thread_cancel LerArquivos:: %d \n",retvalue);
+            retvalue = pthread_cancel(idLerCatalogo);
+            //printf("Retorno / thread_cancel LerCatalogo:: %d \n",retvalue);
 
             return 0;
         };
+        sem_post(&mutexEscrita);
+        sem_post( &shared[0].mutex );
     }
+
 
     return 1;
     
 }
-
